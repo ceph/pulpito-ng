dc9c42cce21ff478e70069ffa5d091d4b6d1ee4f
Zack Cerza <zack@redhat.com>
2024-10-03 10:51:48 -0600

    error handling

--- src/pages/runs/@name/+data.tsx
+++ src/pages/runs/@name/+data.tsx
@@ -5,5 +5,5 @@ import { getURL } from "#src/lib/paddles";
 export default async function data(pageContext: PageContext) {
   const url = getURL(`/runs/${pageContext.routeParams.name}`, pageContext.urlParsed.search);
   const response = await fetch(url);
-  return await response.json();
+  if ( response.ok ) return await response.json();
 }
