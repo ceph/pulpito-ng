14b90d417f687dc022159e46c4f1e96b16eacfc4
Zack Cerza <zack@redhat.com>
2024-10-03 16:07:28 -0600

    FIXME error page

---
+++ src/pages/_error/+Page.tsx
@@ -0,0 +1,40 @@
+import { usePageContext } from 'vike-react/usePageContext'
+
+export function Page() {
+  const pageContext = usePageContext()
+
+  let msg: string // Message shown to the user
+  const { abortReason, abortStatusCode } = pageContext
+  if (abortReason?.notAdmin) {
+    // Handle `throw render(403, { notAdmin: true })`
+    msg = "You cannot access this page because you aren't an administrator."
+  } else if (typeof abortReason === 'string') {
+    // Handle `throw render(abortStatusCode, `You cannot access ${someCustomMessage}`)`
+    msg = abortReason
+  } else if (abortStatusCode === 403) {
+    // Handle `throw render(403)`
+    msg = "You cannot access this page because you don't have enough privileges."
+  } else if (abortStatusCode === 401) {
+    // Handle `throw render(401)`
+    msg = "You cannot access this page because you aren't logged in. Please log in."
+  } else {
+    // Fallback error message
+    msg = pageContext.is404 ?
+      "This page doesn't exist." :
+      "Failed to fetch data; is teuthology-api reachable?"
+  }
+
+  console.log(pageContext)
+  return <p>{msg}</p>
+}
+
+// When using TypeScript you can define the type of `abortReason`
+declare global {
+  namespace Vike {
+    interface PageContext {
+      abortReason?:
+        | string
+        | { notAdmin: true }
+    }
+  }
+}
