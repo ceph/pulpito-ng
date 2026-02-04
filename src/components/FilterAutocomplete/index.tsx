6025d68208e94bd042aa52dc24f8c55789b51221
Zack Cerza <zack@cerza.org>
2026-02-04 16:25:03 -0700

    Rewrite FilterAutocomplete component
    Signed-off-by: Zack Cerza <zack@cerza.org>

--- src/components/FilterAutocomplete/index.tsx
+++ src/components/FilterAutocomplete/index.tsx
@@ -1,55 +1,44 @@
-import { navigate } from 'vike/client/router'
-import TextField from "@mui/material/TextField";
-import Autocomplete from '@mui/material/Autocomplete';
-import type { FilterOptionsState } from "@mui/material/useAutocomplete";
+import React from 'react';
+
+import { Autocomplete } from '@mantine/core';
+
+import { type FilterComponentProps } from '#src/lib/types.d';
 
 
-interface FilterAutocompleteProps {
-  type: string;
-  value: string;
-  baseUrl: string;
-  options: string[];
-}
+export default function FilterAutocomplete(props: FilterComponentProps) {
+  const [value, setValue] = React.useState(props.value)
+  if ( props.id === 'status' ) console.log('value state', props.id, value)
+  React.useEffect(() => {
+    if ( props.id === 'status' ) console.log('useEffect', props.id)
+    const timeout = setTimeout(() => {
+      if ( props.id === 'status' ) console.log('calling onChange', value);
+      props.onChange(value);
+    }, 500)
+    if ( props.id === 'status' ) console.log('timeout', props.id, timeout)
+    return () => clearTimeout(timeout)
+  }, [value]);
+  // const label = props.type.replaceAll("_", " ");
 
-export default function FilterAutocomplete({type, value, baseUrl, options}: FilterAutocompleteProps) {
-  const label = type.replaceAll("_", " ");
-  const onChange = (_: any, newValue: string | null) => {
-    const newUrl = new URL(baseUrl, window.location.origin);
-    if ( newValue ) {
-      newUrl.searchParams.set(type, newValue || "");
+  const options: string[] = [];
+  (props.options || []).forEach(item => {
+    if ( typeof item === 'object' ) {
+      options.push(item.value || '')
     } else {
-      newUrl.searchParams.delete(type);
+      options.push(item)
     }
-    navigate(newUrl.pathname + newUrl.search);
-  };
-  const filterOptions = (options: string[], { inputValue }: FilterOptionsState<string>) => {
-    if (!inputValue) return options;
-    let result: string[] = [];
-    result.push(...options.filter((item) => item === inputValue));
-    result.push(
-      ...options.filter((item) => {
-        if (result.indexOf(item) !== -1) return false;
-        return item.startsWith(inputValue);
-      })
-    );
-    result.push(
-      ...options.filter((item) => {
-        if (result.indexOf(item) !== -1) return false;
-        return item.includes(inputValue);
-      })
-    );
-    return result;
-  };
 
+  })
   return (
     <Autocomplete
-      value={value || null}
-      filterOptions={filterOptions}
-      autoHighlight
-      onChange={onChange}
-      options={options}
-      renderInput={(params) => <TextField {...params} label={label} />}
-      size="small"
+      label={props.label}
+      value={value}
+      onChange={setValue}
+      data={options}
+      size="sm"
+      comboboxProps={{ withinPortal: false }}
+      inputSize={props.size}
+      clearable
+      autoSelectOnBlur
     />
   );
 }
